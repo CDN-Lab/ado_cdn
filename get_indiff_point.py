deee47e5c16fee627d8f73a00cc8065657e9f641
cdn-pizarro <ricardo.pizarro@nih.gov>
2023-07-10 14:43:54 -0400

    adding ADO type LL functions

--- get_indiff_point.py
+++ get_indiff_point.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import os,sys
 import math
-
+from scipy.stats import bernoulli
+# expit(x) = 1/(1+exp(-x))
+from scipy.special import expit as inv_logit
 
 
 def set_grids():
@@ -57,7 +59,6 @@ def make_grid(design):
     return grid_df
 
 
-
 def populate_log_lik(choice_set,param_set,response_set):
 
     n_c = choice_set.shape[0]
@@ -84,30 +85,41 @@ def compute_log_lik(row_c,row_p,row_r):
     return LL
 
 def SV_discount(value,delay,kappa=0.005,alpha=1.0):
-    SV = (value**alpha)/(1+kappa*delay)
+    # SV = (value**alpha)/(1+kappa*delay)
+    def discount(delay):
+        return np.divide(1, 1 + kappa * delay)
+    SV = (value**alpha) * discount(delay)
     return SV
 
 def prob_softmax(SV1,SV0,gamma=0.5):
     # compute probability using softmax function, return 0 if OverlowError is thrown
     try: 
-        p = 1 / (1 + math.exp(-gamma*(SV1 - SV0)))
+        p = inv_logit(gamma*(SV1-SV0))
+        # p = 1 / (1 + math.exp(-gamma*(SV1 - SV0)))
     except OverflowError:
         p = 0
     return p
 
 def get_LL(choice,p_choose_reward):
-    eps = np.finfo(float).eps
+    # eps = np.finfo(float).eps
     # smallest value LL can take
-    LL_floor = -200
+    LL_floor = np.log(1e-7)
     try:
         LL = (choice==1)*np.log(p_choose_reward) + ((choice==0))*np.log(1-p_choose_reward)
+        LL_bernie = bernoulli.logpmf(choice, p_choose_reward)
+        print('LL is {} and bernie is {}'.format(LL,LL_bernie))
+
         if math.isnan(LL):
             # print('log_lik is NaN with (choice,prob):({},{})'.format(choice,p_choose_reward))
             LL = LL_floor
         elif LL == float("-inf"):
             # print('log_lik is -inf with (choice,prob):({},{})'.format(choice,p_choose_reward)) 
             LL = LL_floor
+        elif LL < LL_floor:
+            # print('We got small LL: {}'.format(LL))
+            LL = LL_floor
     except RuntimeWarning:
+        print('We got some RunTimeWarning up in here?')
         LL = LL_floor
     return LL
 
@@ -122,11 +134,9 @@ log_lik = populate_log_lik(choice_set,param_set,response_set)
 
 
 print(log_lik[0,:,0].shape)
-print(log_lik[0,:,0])
-
-print(np.min(np.min(log_lik)))
-
-print(np.log(np.finfo(float).eps))
+# print(log_lik[0,:,0])
+# print(np.min(np.min(log_lik)))
+# print(np.log(np.finfo(float).eps))
 
 '''
 print(choice_set)
